c46b542de95a32446e65ade14ab49b3363d25fdc
Lukas Folkman <lukas.folkman@gmail.com>
2024-06-18 14:59:02 +1000

    add check videos before submit

---
+++ check_videos_exist.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DATA_DIR="../data"
+VIDEO_LIST="VIDEO_LIST.txt"
+
+while read -r video_path
+do
+  if [[ -n "${video_path}" ]]; then
+    if [[ ! -f "${DATA_DIR}/${video_path}" ]]; then
+      echo "ERROR: ${DATA_DIR}/${video_path} does not exist"
+    fi
+  fi
+done < "${VIDEO_LIST}"
+
+echo "Done."

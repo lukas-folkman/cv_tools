9198e6fc4654029c7c74b00874e2ab2392893d36
Lukas Folkman <lukas.folkman@gmail.com>
2024-06-19 10:04:54 +1000

    check dirs

--- check_videos_exist.sh
+++ check_videos_exist.sh
@@ -6,7 +6,7 @@ VIDEO_LIST="VIDEO_LIST.txt"
 while read -r video_path
 do
   if [[ -n "${video_path}" ]]; then
-    if [[ ! -f "${DATA_DIR}/${video_path}" ]]; then
+    if [[ ! -f "${DATA_DIR}/${video_path}" && ! -d "${DATA_DIR}/${video_path}" ]]; then
       echo "ERROR: ${DATA_DIR}/${video_path} does not exist"
     fi
   fi

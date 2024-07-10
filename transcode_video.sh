95b3e5d4e21452c4097197b6a4d9f50e5573b8d4
Lukas Folkman <lukas.folkman@gmail.com>
2024-07-10 12:44:05 +1000

    better and HPC friendly transcode_video.sh

--- transcode_video.sh
+++ transcode_video.sh
@@ -7,18 +7,36 @@ if [[ ${vlc_opt} == "--vlc" ]]; then
   VLC=${vlc_bin}
   shift 2
 else
-  VLC="/Applications/VLC.app/Contents/MacOS/VLC"
+  module load misc/vlc/vlc 2> /dev/null
+  if [[ ${?} == 0 ]]; then
+    VLC="vlc"
+  else
+    VLC="/Applications/VLC.app/Contents/MacOS/VLC"
+  fi
 fi
 
-if [[ ! -f ${VLC} ]]; then
+output_opt="$1"
+output_dir="$2"
+
+if [[ ${output_opt} == "--output_dir" ]]; then
+  shift 2
+else
+  output_dir=""
+fi
+
+if [[ ! -f ${VLC} && ${VLC} != "vlc" ]]; then
   echo "ERROR: ${VLC} does not exist. Specify a valid VLC binary, e.g.:"
   echo "$ bash transcode_video.sh --vlc $HOME/VideoLAN/VLC video1.asf video2.asf"
+elif [[ -n ${output_dir} && ! -d ${output_dir} ]]; then
+  echo "ERROR: Output directory ${output_dir} does not exist."
 else
   for input in "$@"
   do
-    output=`echo $input | sed 's/\.[A-Za-z0-9]*$/.mp4/'`
-    if [[ -f ${output} ]]; then
-      output=`echo $input | sed 's/\.[A-Za-z0-9]*$/_transcoded.mp4/'`
+    output=$(echo ${input} | sed 's/\.[A-Za-z0-9]*$/.mp4/')
+    if [[ -n ${output_dir} ]]; then
+      output=${output_dir}/$(basename "${output}")
+    elif [[ -f ${output} ]]; then
+      output=$(echo ${input} | sed 's/\.[A-Za-z0-9]*$/_transcoded.mp4/')
     fi
 
     if [[ -f ${output} ]]; then
